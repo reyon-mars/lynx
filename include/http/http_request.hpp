124f35d5484204f9585201e7cb2e0d1673592df4
Reyon Rai <reyonrai50@gmail.com>
2026-03-06 16:14:49 +0545

    minor improvement in the http request class.

--- include/http/http_request.hpp
+++ include/http/http_request.hpp
@@ -1,41 +1,41 @@
 #pragma once
+#include "http/http_types.hpp"
 #include <cstddef>
-#include <map>
 #include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
-
-constexpr size_t REQ_LINE_SIZE = 3;
 
 namespace http
 {
 	class http_request
 	{
 	private:
-		std::string method_;
-		std::string uri_;
-		std::string version_;
-		std::map<std::string, std::string> headers_;
-		std::vector<std::byte> body_;
+		request_line request_line_;
+		headers headers_;
+		http_body body_;
 
 	public:
+		http_request() = default;
+
 		void set_method(std::string method);
 		void set_uri(std::string uri);
 		void set_version(std::string version);
 
-		void add_header(std::string name, std::string value);
+		void add_header(std::string_view name, std::string value);
+		void remove_header(std::string_view name);
+		bool has_header(std::string_view name) const;
+
+		void set_body(std::string_view body);
 		void set_body(std::vector<std::byte> body);
 
 		const std::string& method() const;
 		const std::string& uri() const;
 		const std::string& version() const;
-		std::optional<std::string> get_header(std::string_view name) const;
+		std::optional<std::string_view> get_header(std::string_view name) const;
 		const std::vector<std::byte>& body() const;
 
 		bool has_body() const;
 		size_t content_length() const;
-
-
 	};
 } // namespace http
---
+++ src/http/http_request.cpp
@@ -0,0 +1,102 @@
+#include "http/http_request.hpp"
+#include "http/http_types.hpp"
+#include "utils/string_utils.hpp"
+#include <algorithm>
+#include <cctype>
+#include <cstddef>
+#include <optional>
+#include <span>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+namespace http
+{
+	void http_request::set_method(std::string method)
+	{
+		request_line_.method_ = std::move(method);
+	}
+
+	void http_request::set_uri(std::string uri)
+	{
+		request_line_.uri_ = std::move(uri);
+	}
+
+	void http_request::set_version(std::string version)
+	{
+		request_line_.version_ = std::move(version);
+	}
+
+	void http_request::add_header(std::string_view name, std::string value)
+	{
+		std::string normalized_name = utils::to_lower(name);
+		headers_.insert_or_assign(normalized_name, std::move(value));
+	}
+
+	void http_request::remove_header(std::string_view name)
+	{
+		std::string normalized_name = utils::to_lower(name);
+		headers_.erase(normalized_name);
+	}
+
+	bool http_request::has_header(std::string_view name) const
+	{
+		std::string normalized_name = utils::to_lower(name);
+		return headers_.contains(normalized_name);
+	}
+
+	void http_request::set_body(std::string_view body)
+	{
+		auto view = std::as_bytes(std::span(body));
+		body_.assign(view.begin(), view.end());
+		add_header("Content-Length", std::to_string(body_.size()));
+	}
+
+	void http_request::set_body(std::vector<std::byte> body)
+	{
+		body_ = std::move(body);
+		add_header("Content-Length", std::to_string(body_.size()));
+	}
+
+	const std::string& http_request::method() const
+	{
+		return request_line_.method_;
+	}
+
+	const std::string& http_request::uri() const
+	{
+		return request_line_.uri_;
+	}
+
+	const std::string& http_request::version() const
+	{
+		return request_line_.version_;
+	}
+
+	std::optional<std::string_view> http_request::get_header(std::string_view name) const
+	{
+		auto it = headers_.find(utils::to_lower(name));
+		if (it != headers_.end())
+		{
+			return it->second;
+		}
+		return std::nullopt;
+	}
+
+	const http_body& http_request::body() const
+	{
+		return body_;
+	}
+
+	bool http_request::has_body() const
+	{
+		return !body_.empty();
+	}
+
+	size_t http_request::content_length() const
+	{
+		return body_.size();
+	}
+
+} // namespace http
