1f28700acca70189d04b2ce43abe2bcfa10915c7
Reyon Rai <reyonrai50@gmail.com>
2026-03-07 20:20:30 +0545

    updated the main to use the new http_handle_client.

--- src/main_tui.cpp
+++ src/main_tui.cpp
@@ -1,3 +1,6 @@
+#include "http/http_response.hpp"
+#include "http/request_context.hpp"
+#include "http/router.hpp"
 #include "net/connection.hpp"
 #include "net/connection_stats.hpp"
 #include "net/net_except.hpp"
@@ -28,6 +31,63 @@ int main()
 		http_server.bind(config.sock_addr);
 		http_server.listen();
 
+		http::router router_;
+
+		router_.add_route("GET",
+						  "/",
+						  [](const http::request_context&)
+						  {
+							  http::http_response response;
+							  response.set_status(200);
+							  response.set_header("Content-Type", "text/html");
+							  response.set_body("<h1> Welcome to my server. </h1> ");
+							  return response;
+						  });
+
+		router_.add_route("GET",
+						  "/hello",
+						  [](const http::request_context& ctx)
+						  {
+							  http::http_response response;
+							  response.set_status(200);
+
+							  std::string name = "World";
+							  if (ctx.uri_info.params.contains("name"))
+							  {
+								  name = ctx.uri_info.params.at("name");
+							  }
+
+							  response.set_body("Hello, " + name + "!");
+							  return response;
+						  });
+
+		router_.add_route("GET",
+						  "/users/:id",
+						  [](const http::request_context& ctx)
+						  {
+							  http::http_response resp;
+							  resp.set_status(200);
+
+							  std::string user_id = ctx.params.at("id");
+							  resp.set_body("User ID: " + user_id);
+							  return resp;
+						  });
+
+		router_.add_route("POST",
+						  "/users",
+						  [](const http::request_context& ctx)
+						  {
+							  http::http_response resp;
+							  resp.set_status(201);
+
+							  // Access POST body
+							  const auto& body = ctx.request.body();
+							  std::string body_str(reinterpret_cast<const char*>(body.data()), body.size());
+
+							  resp.set_body("Created user with data: " + body_str);
+							  return resp;
+						  });
+
 		std::cout << "HTTP Server listening on " << config.sock_addr.ip << ":" << config.sock_addr.port << "\n";
 
 		while (true)
@@ -37,15 +97,15 @@ int main()
 				net::Socket client = http_server.accept();
 				utils::logger::log("New client connected.");
 
-				auto task = [client_socket = std::move(client)]() mutable
+				auto task = [client_socket = std::move(client), &router_]() mutable
 				{
 					net::connection_stats stats;
 					utils::logger::log("Total Active Connections: " + std::to_string(stats.get_active_conn()));
 
-					net::handle_http_client(std::move(client_socket));
+					net::handle_http_client(std::move(client_socket), router_);
 
 					utils::logger::log("Connection closed. Active: " + std::to_string(stats.get_active_conn() - 1));
-					utils::logger::log("Total Connections Served: " + std::to_string(stats.get_total_conn()) );
+					utils::logger::log("Total Connections Served: " + std::to_string(stats.get_total_conn()));
 				};
 
 				t_pool.submit(std::move(task));
